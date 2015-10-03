f9133ab50cbfc115152c99f3ce38523c2e5e38e2
Kazuya <IgYmB0218LP@gmail.com>
2015-10-03 11:29:00 +0900

    change test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,33 +9,42 @@ describe "Static pages" do
 
   subject { page }
 
-  describe "Home page" do
-    before {visit root_path}
+  shared_examples_for "all static pages" do
+    it {should have_content(heading)}
+    it {should have_title(full_title(page_title))}
+  end
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+  describe "Home page" do
+    before { visit root_path }
+
+    # it { should have_content('Sample App') }
+    # it { should have_title(full_title('')) }
+    let(:heading)         { 'Sample App' }
+    let(:page_title)      { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home')}
+
   end
 
   describe "Help pages" do
     before {visit help_path}
 
-    it { should have_content('Help') }
-
-    it { should have_title(full_title('Help')) }
+    let(:heading)         { 'Help' }
+    let(:page_title)      { 'Help' }
   end
 
   describe "About Page" do
     before {visit about_path}
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading)         { 'About' }
+    let(:page_title)      { 'About Us' }
   end
 
   describe "Contact Page" do
     before {visit contact_path}
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact'))}
+    let(:heading)         { 'Contact' }
+    let(:page_title)      { 'Contact' }
   end
 end
