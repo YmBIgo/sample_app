727aee49004e2d2612f35ab879e8b620a047e8ae
Kazuya <IgYmB0218LP@gmail.com>
2015-10-09 18:31:12 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,20 @@ module SessionsHelper
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
-    # ||= とは、@current_user = @current_user || User.findby... と同じ
+    # ||= とは、@current_user = @current_user || User.find_by... と同じ
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default )
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
 end
